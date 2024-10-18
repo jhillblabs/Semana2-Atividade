64241503f3161b54c8c027ad0d76641d4c3c912c
jhillblabs <jhilzinhavendito@gamil.com>
2024-10-17 22:28:55 -0300

    teste1

---
+++ atividade Imagens1/imagens.js
@@ -0,0 +1,15 @@
+var num = Math.floor(Math.random() * 5);
+var imagens = [
+    'img/desenvolviSistemas.jpg',
+    'img/bancoDados.webp',
+    'img/linguagem.jpeg',
+    'img/seguranca.jpg',
+    'img/internetProtocolos.jpg',
+    'img/mobileGanyu.jpg',
+    'img/desenvolvitcc.jpg',
+];
+var visualizar = imagens[num];
+document.write('<h1> APERTE F5 </h1>');
+document.write(`<img src="${visualizar}">`);
+
+var banner = document.getElementById("banner");
